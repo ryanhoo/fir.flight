0736f9e0e4ffc80fe591690490e2a42abe107cf1
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-15 20:25:56 +0800

    init(repo): Basic structure

---
+++ app/src/main/java/io/github/ryanhoo/firFlight/FlightApplication.java
@@ -0,0 +1,18 @@
+package io.github.ryanhoo.firFlight;
+
+import android.app.Application;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/15/16
+ * Time: 5:47 PM
+ * Desc: FlightApplication
+ */
+public class FlightApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/io/github/ryanhoo/firFlight/ui/BaseActivity.java
@@ -0,0 +1,20 @@
+package io.github.ryanhoo.firFlight.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/15/16
+ * Time: 8:12 PM
+ * Desc: BaseActivity
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
