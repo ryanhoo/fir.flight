ace0583418544d545a0878ffb571cc6cf12b6338
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-16 00:11:56 +0800

    feat(Network): Retrofit RESTful network service interfaces

---
+++ app/src/main/java/io/github/ryanhoo/firFlight/data/service/RetrofitService.java
@@ -0,0 +1,26 @@
+package io.github.ryanhoo.firFlight.data.service;
+
+import io.github.ryanhoo.firFlight.data.model.Token;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/15/16
+ * Time: 11:06 PM
+ * Desc: RetrofitService
+ */
+public interface RetrofitService {
+
+    // Token
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<Token> login(@Field("email") String email, @Field("password") String password);
+
+    @GET("/user/api_token")
+    Call<Token> apiToken(@Query("access_token") String accessToken);
+
+    // User
+}
