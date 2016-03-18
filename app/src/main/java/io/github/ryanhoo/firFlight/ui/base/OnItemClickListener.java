06e03c97f8a9e0f3479feb1717bca7ca45c36de4
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-18 23:30:17 +0800

    feat(Base): Common OnItemClickListener for RecyclerView & Adapter

---
+++ app/src/main/java/io/github/ryanhoo/firFlight/ui/base/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package io.github.ryanhoo.firFlight.ui.base;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/18/16
+ * Time: 10:52 PM
+ * Desc: OnItemClickListener
+ */
+public interface OnItemClickListener<T> {
+
+    void onItemClick(T item, int position);
+}
