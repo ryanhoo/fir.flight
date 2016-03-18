c629fbf28410c4619252911a0312ab3fc2f49015
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-18 23:23:17 +0800

    feat(BaseActivity): Support homeAsUp in an easy way

--- app/src/main/java/io/github/ryanhoo/firFlight/ui/base/BaseActivity.java
+++ app/src/main/java/io/github/ryanhoo/firFlight/ui/base/BaseActivity.java
@@ -2,7 +2,10 @@ package io.github.ryanhoo.firFlight.ui.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created with Android Studio.
@@ -16,5 +19,31 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * An easy way to set up non-home(no back button on the toolbar) activity to enable
+     * go back action.
+     *
+     * @param toolbar The toolbar with go back button
+     * @return ActionBar
+     */
+    protected ActionBar supportActionBar(Toolbar toolbar) {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+        return actionBar;
     }
 }
