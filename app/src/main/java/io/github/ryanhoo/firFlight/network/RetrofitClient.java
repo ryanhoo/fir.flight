07fd2b3c8365baeb842809dc950a42b3425ce944
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-19 23:39:33 +0800

    feat(Json Deserialize): Add custom JsonDeserializer for special timestamps from fir's API

--- app/src/main/java/io/github/ryanhoo/firFlight/network/RetrofitClient.java
+++ app/src/main/java/io/github/ryanhoo/firFlight/network/RetrofitClient.java
@@ -1,7 +1,12 @@
 package io.github.ryanhoo.firFlight.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.github.ryanhoo.firFlight.network.gson.DateDeserializer;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.Date;
 
 /**
  * Created with Android Studio.
@@ -15,7 +20,13 @@ public class RetrofitClient {
     public static Retrofit defaultInstance() {
         return new Retrofit.Builder()
                 .baseUrl(ServerConfig.API_HOST)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(defaultGson()))
                 .build();
+    }
+
+    public static Gson defaultGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(Date.class, new DateDeserializer())
+                .create();
     }
 }
---
+++ app/src/main/java/io/github/ryanhoo/firFlight/network/gson/DateDeserializer.java
@@ -0,0 +1,54 @@
+package io.github.ryanhoo.firFlight.network.gson;
+
+import android.util.Log;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.util.Date;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/19/16
+ * Time: 9:35 PM
+ * Desc: DateDeserializer
+ */
+public class DateDeserializer implements JsonDeserializer<Date> {
+
+    private static final String TAG = "DateDeserializer";
+
+    // 1455859798.276
+    static final String DATE_WITH_MILLIS_3 = "\\d{10}\\.\\d{3}";
+
+    // 1453271675.02
+    static final String DATE_WITH_MILLIS_2 = "\\d{10}\\.\\d{2}";
+
+    // 1455859798
+    static final String DATE_WITH_SECONDS = "\\d{10}";
+
+    @Override
+    public Date deserialize(JsonElement jsonElement, Type type,
+                            JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        try {
+            String dateString = jsonElement.getAsString();
+            if (dateString.matches(DATE_WITH_MILLIS_3)) {
+                long timestamp = Long.parseLong(dateString.replace(".", ""));
+                return new Date(timestamp);
+            } else if (dateString.matches(DATE_WITH_SECONDS)) {
+                long timestamp = Long.parseLong(dateString);
+                return new Date(timestamp * 1000);
+            } else if (dateString.matches(DATE_WITH_MILLIS_2)) {
+                long timestamp = Long.parseLong(dateString.replace(".", ""));
+                return new Date(timestamp * 10);
+            } else {
+                Log.e(TAG, "Unable to parse date " + jsonElement);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "deserialize: " + jsonElement, e);
+        }
+        return null;
+    }
+}
