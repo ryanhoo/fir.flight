ca92c6d405f2c268d094a17f3252e4f2f741e5f8
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-19 23:41:55 +0800

    feat(Util): Extract urls from html text with unit test

---
+++ app/src/main/java/io/github/ryanhoo/firFlight/util/HtmlUtils.java
@@ -0,0 +1,29 @@
+package io.github.ryanhoo.firFlight.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/19/16
+ * Time: 10:08 PM
+ * Desc: HtmlUtils
+ */
+public class HtmlUtils {
+
+    public static final String REGEX_URL = "http(s)?://(.[^\"><])*";
+
+    public static List<String> getUrlsFromHtml(String htmlText) {
+        List<String> urls = new ArrayList<>();
+        Pattern p = Pattern.compile(REGEX_URL);
+        Matcher m = p.matcher(htmlText);
+        while (m.find()) {
+            String urlStr = m.group();
+            urls.add(urlStr);
+        }
+        return urls;
+    }
+}
---
+++ app/src/test/java/io/github/ryanhoo/firFlight/UtilsTest.java
@@ -0,0 +1,27 @@
+package io.github.ryanhoo.firFlight;
+
+import io.github.ryanhoo.firFlight.util.HtmlUtils;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/19/16
+ * Time: 10:26 PM
+ * Desc: UtilsTest
+ */
+public class UtilsTest {
+
+    @Test
+    public void testGetUrlsFromHtml() {
+        final String html = "<p><a href=\\\"http://blog.fir.im/turbo-qiniu/\"><font color=\"#3AB2A7\" size=6px >&quot;一行命令 优化上传速度&quot;</font><font color=\"#33";
+        List<String> urls = HtmlUtils.getUrlsFromHtml(html);
+        for (String url : urls) {
+            System.out.println("url: " + url);
+        }
+        Assert.assertEquals(urls.size(), 1);
+    }
+}
