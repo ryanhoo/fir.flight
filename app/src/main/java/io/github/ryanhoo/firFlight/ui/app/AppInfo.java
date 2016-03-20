4335c8ffbc4f9dd274a9627c3c3bf0b70fe028e2
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-20 16:59:08 +0800

    feat(App Info): Get app info by package name
    - isInstalled
    - isUpToDate
    - launchIntent

---
+++ app/src/main/java/io/github/ryanhoo/firFlight/ui/app/AppInfo.java
@@ -0,0 +1,53 @@
+package io.github.ryanhoo.firFlight.ui.app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import io.github.ryanhoo.firFlight.data.model.App;
+import io.github.ryanhoo.firFlight.data.model.Release;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/20/16
+ * Time: 4:19 PM
+ * Desc: AppInfo
+ */
+public class AppInfo {
+
+    private static final String TAG = "AppInfo";
+
+    public boolean isInstalled;
+    public boolean isUpToDate;
+
+    public Intent launchIntent;
+
+    public App app;
+
+    public AppInfo(Context context, App app) {
+        Release onlineRelease = app.getMasterRelease();
+        String packageName = app.getBundleId();
+        String build = onlineRelease.getBuild();
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_META_DATA);
+            if (packageInfo == null) {
+                isInstalled = false;
+                isUpToDate = false;
+            } else {
+                isInstalled = true;
+                int onlineVersionCode = Integer.parseInt(build);
+                int versionCode = packageInfo.versionCode;
+                isUpToDate = versionCode >= onlineVersionCode;
+                if (isUpToDate) {
+                    // http://stackoverflow.com/a/3422824/2290191
+                    launchIntent = packageManager.getLaunchIntentForPackage(packageName);
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, String.format("isAppUpToDate %s: [%s, %s]", app.getName(), packageName, build), e);
+        }
+    }
+}
---
+++ app/src/main/java/io/github/ryanhoo/firFlight/util/AppUtils.java
@@ -0,0 +1,49 @@
+package io.github.ryanhoo.firFlight.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import io.github.ryanhoo.firFlight.data.model.App;
+import io.github.ryanhoo.firFlight.data.model.Release;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/20/16
+ * Time: 4:01 PM
+ * Desc: AppUtils
+ */
+public class AppUtils {
+
+    private static final String TAG = "AppUtils";
+
+    public static boolean isAppInstalled(Context context, App app) {
+        Release onlineRelease = app.getMasterRelease();
+        String packageName = app.getBundleId();
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(packageName, PackageManager.GET_META_DATA);
+            return packageInfo != null;
+        } catch (Exception e) {
+            Log.e(TAG, "isAppInstalled: " + packageName, e);
+        }
+        return false;
+    }
+
+    public static boolean isAppUpToDate(Context context, App app) {
+        Release onlineRelease = app.getMasterRelease();
+        String packageName = app.getBundleId();
+        String build = onlineRelease.getBuild();
+        try {
+            int onlineVersionCode = Integer.parseInt(build);
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(packageName, PackageManager.GET_META_DATA);
+            int versionCode = packageInfo.versionCode;
+            return versionCode >= onlineVersionCode;
+        } catch (Exception e) {
+            Log.e(TAG, String.format("isAppUpToDate %s: [%s, %s]", app.getName(), packageName, build), e);
+        }
+        return false;
+    }
+}
