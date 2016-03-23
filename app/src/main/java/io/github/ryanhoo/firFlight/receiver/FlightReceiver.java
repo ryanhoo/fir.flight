b78324702b2dd95291b131a00802267c2d92a712
ryanhoo <ryan.hoo.j@gmail.com>
2016-03-23 22:31:47 +0800

    feat(Auto Update): Auto update app info and show notification to notify user

---
+++ app/src/main/java/io/github/ryanhoo/firFlight/receiver/FlightReceiver.java
@@ -0,0 +1,31 @@
+package io.github.ryanhoo.firFlight.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import io.github.ryanhoo.firFlight.service.FlightService;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/23/16
+ * Time: 8:43 PM
+ * Desc: FlightReceiver
+ */
+public class FlightReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "FlightReceiver";
+
+    public static final String ACTION_UPDATE = "io.github.ryanhoo.action.UPDATE";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: " + intent.getAction());
+        // if (ACTION_UPDATE.equals(intent.getAction())) {
+        // Start update service
+        Intent serviceIntent = new Intent(context, FlightService.class);
+        context.startService(serviceIntent);
+        // }
+    }
+}
---
+++ app/src/main/java/io/github/ryanhoo/firFlight/service/FlightService.java
@@ -0,0 +1,88 @@
+package io.github.ryanhoo.firFlight.service;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+import io.github.ryanhoo.firFlight.R;
+import io.github.ryanhoo.firFlight.data.UserSession;
+import io.github.ryanhoo.firFlight.data.model.App;
+import io.github.ryanhoo.firFlight.data.service.RetrofitService;
+import io.github.ryanhoo.firFlight.network.RetrofitClient;
+import io.github.ryanhoo.firFlight.ui.app.AppInfo;
+import io.github.ryanhoo.firFlight.ui.main.MainActivity;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 3/23/16
+ * Time: 8:35 PM
+ * Desc: FlightService
+ * - https://guides.codepath.com/android/Managing-Threads-and-Custom-Services#threading-within-the-service
+ * - https://guides.codepath.com/android/Starting-Background-Services
+ */
+public class FlightService extends IntentService {
+
+    private static final String TAG = "FlightService";
+
+    NotificationManager notificationManager;
+
+    public FlightService() {
+        super("FlightService");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: on thread #" + Thread.currentThread().getName());
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent: " + intent.getAction() + " on thread #" + Thread.currentThread().getName());
+        if (!UserSession.getInstance().isSignedIn()) return;
+
+        RetrofitService retrofitService = RetrofitClient.defaultInstance().create(RetrofitService.class);
+        Call<List<App>> call = retrofitService.apps(UserSession.getInstance().getToken().getAccessToken());
+        try {
+            Response<List<App>> response = call.execute();
+            List<App> apps = response.body();
+            for (final App app : apps) {
+                AppInfo appInfo = new AppInfo(this, app);
+                if (appInfo.isInstalled && !appInfo.isUpToDate) {
+                    onAppNewVersion(app);
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Request app list: " + call.request().url(), e);
+        }
+    }
+
+    private void onAppNewVersion(App app) {
+        // Notification
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        String title = app.getName();
+        String content = String.format("New version %s(%s)",
+                app.getMasterRelease().getVersion(), app.getMasterRelease().getBuild());
+        Log.d(TAG, String.format("%s \t %s", title, content));
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+        int notificationId = app.getId().hashCode();
+        notificationManager.notify(notificationId, notification);
+    }
+}
