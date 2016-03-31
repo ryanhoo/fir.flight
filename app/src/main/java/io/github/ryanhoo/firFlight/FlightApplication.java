935d500734003266c89974ce5db7ff12df276235
ryanhoo <ryan.hoo.j@gmail.com>
2016-04-01 03:55:49 +0800

    feat(Bug Report): Integrate Fabric to collect crash reports

--- app/src/main/java/io/github/ryanhoo/firFlight/FlightApplication.java
+++ app/src/main/java/io/github/ryanhoo/firFlight/FlightApplication.java
@@ -1,7 +1,9 @@
 package io.github.ryanhoo.firFlight;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
 import im.fir.sdk.FIR;
+import io.fabric.sdk.android.Fabric;
 import io.github.ryanhoo.firFlight.data.UserSession;
 
 /**
@@ -16,6 +18,8 @@ public class FlightApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Crashlytics
+        Fabric.with(this, new Crashlytics());
         // BugHD
         FIR.init(this);
 
