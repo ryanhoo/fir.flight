ccb0cb69e26af7bea005d896c5e4358443cb1c3d
ryanhoo <ryan.hoo.j@gmail.com>
2016-06-26 00:16:21 +0800

    refactor(DbHelper): Register tables

--- app/src/main/java/io/github/ryanhoo/firFlight/data/source/local/DatabaseHelper.java
+++ app/src/main/java/io/github/ryanhoo/firFlight/data/source/local/DatabaseHelper.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 import io.github.ryanhoo.firFlight.data.source.local.tables.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,10 +20,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String TAG = "DatabaseHelper";
 
-    public static final int DATABASE_VERSION = 20;
+    public static final int DATABASE_VERSION = 23;
     public static final String DATABASE_NAME = "fir-flight.db";
 
     List<Class<? extends BaseTable>> mRegisteredTables;
+
+    /**
+     * Register tables here
+     * */
+    private void registerTables() {
+        mRegisteredTables = new ArrayList<>();
+        mRegisteredTables.add(AppTable.class);
+        mRegisteredTables.add(ReleaseTable.class);
+        mRegisteredTables.add(MessageTable.class);
+        mRegisteredTables.add(UserTable.class);
+        mRegisteredTables.add(TokenTable.class);
+    }
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -36,11 +49,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         } catch (Exception e) {
             Log.e(TAG, "onCreate: ", e);
         }
-        db.execSQL(UserTable.CREATE_TABLE);
-        db.execSQL(TokenTable.CREATE_TABLE);
-        db.execSQL(AppTable.CREATE_TABLE);
-        db.execSQL(ReleaseTable.CREATE_TABLE);
-        db.execSQL(MessageTable.CREATE_TABLE);
     }
 
     @Override
@@ -60,30 +68,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         } catch (Exception e) {
             Log.e(TAG, "onDelete: ", e);
         }
-        db.execSQL(UserTable.DELETE_TABLE);
-        db.execSQL(TokenTable.DELETE_TABLE);
-        db.execSQL(AppTable.DELETE_TABLE);
-        db.execSQL(ReleaseTable.DELETE_TABLE);
-        db.execSQL(MessageTable.DELETE_TABLE);
     }
 
-    // Tables
-
-    private void registerTables() {
-        //        mRegisteredTables = new ArrayList<>();
-        //        mRegisteredTables.add(AppTable.class);
-        //        mRegisteredTables.add(ReleaseTable.class);
-        //        mRegisteredTables.add(AppReleaseTable.class);
-        //        mRegisteredTables.add(MessageTable.class);
-        //        mRegisteredTables.add(UserTable.class);
-        //        mRegisteredTables.add(TokenTable.class);
-    }
+    // Tables creation & deletion
 
     private void createTables(SQLiteDatabase db) throws IllegalAccessException, InstantiationException {
         if (mRegisteredTables == null) return;
         for (Class<? extends BaseTable> table : mRegisteredTables) {
             BaseTable tableInstance = table.newInstance();
-            db.execSQL(tableInstance.createTable());
+            db.execSQL(tableInstance.createTableSql());
         }
     }
 
@@ -91,7 +84,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if (mRegisteredTables == null) return;
         for (Class<? extends BaseTable> table : mRegisteredTables) {
             BaseTable tableInstance = table.newInstance();
-            db.execSQL(tableInstance.deleteTable());
+            db.execSQL(tableInstance.deleteTableSql());
         }
     }
 }
