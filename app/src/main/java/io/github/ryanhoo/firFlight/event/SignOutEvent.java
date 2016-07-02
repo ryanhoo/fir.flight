8c5e8495845dc99b69c5f6c763345bbfb077933e
ryanhoo <ryan.hoo.j@gmail.com>
2016-07-03 00:04:37 +0800

    feat(Events): RxBus events

---
+++ app/src/main/java/io/github/ryanhoo/firFlight/event/SignOutEvent.java
@@ -0,0 +1,12 @@
+package io.github.ryanhoo.firFlight.event;
+
+/**
+ * Created with Android Studio.
+ * User: ryan@whitedew.me
+ * Date: 7/2/16
+ * Time: 11:57 PM
+ * Desc: SignOutEvent
+ */
+public class SignOutEvent {
+
+}
